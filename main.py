78930fc4fb7f3f6423a73f2f7cc539e7cd30d253
lluansous <luansousa@protonmail.com>
2020-11-26 02:56:30 +0000

    Mensagem adicionada!

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE O SEU CÓDIGO AQUI.
 
+    print("Hello There!")
+
+
+
 
 if __name__ == '__main__':
     main()
